4cc0c609a8ab0435fbc2210c03714a430f7787cb
sujeong <tnwjd6496@naver.com>
2021-10-27 03:30:26 +0900

    신분증 인증 뷰 구현

--- wupitch/wupitch/Screen/SignUp/Controllers/SignUpFifthVC.swift
+++ wupitch/wupitch/Screen/SignUp/Controllers/SignUpFifthVC.swift
@@ -9,12 +9,28 @@ import UIKit
 
 class SignUpFifthVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var backBtn: UIButton!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var photoBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setStyle()
+    }
+    func setStyle() {
+        titleLabel.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 24.adjusted)
+        titleLabel.setTextWithLineHeight(text: titleLabel.text, lineHeight: 30.adjusted)
+        titleLabel.textColor = .black
         
+        descriptionLabel.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 12.adjusted)
+        descriptionLabel.setTextWithLineHeight(text: descriptionLabel.text, lineHeight: 20.adjusted)
+        descriptionLabel.textColor = .gray02
+        
+        photoBtn.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Bold", size: 16.adjusted)
+        photoBtn.layer.cornerRadius = 8
     }
     
     @IBAction func touchUpPhotoBtn(_ sender: Any) {
