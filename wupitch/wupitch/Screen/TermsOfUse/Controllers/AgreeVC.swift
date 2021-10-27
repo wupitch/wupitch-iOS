dae528a77612e04827640ce52f47881034c5270d
sujeong <tnwjd6496@naver.com>
2021-10-28 04:07:47 +0900

    약관보기 및 개인정보 수집 및 이용 보기 뷰 수정

--- wupitch/wupitch/Screen/TermsOfUse/Controllers/AgreeVC.swift
+++ wupitch/wupitch/Screen/TermsOfUse/Controllers/AgreeVC.swift
@@ -9,21 +9,13 @@ import UIKit
 
 class AgreeVC: UIViewController {
     
-    @IBOutlet weak var contentCV: UICollectionView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var backBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setDelegate()
-        setStyle()
-    }
     
-    func setDelegate() {
-        contentCV.delegate = self
-        contentCV.dataSource = self
-        contentCV.register(AgreeCVCell.nib(), forCellWithReuseIdentifier: AgreeCVCell.identifier)
+        setStyle()
     }
     
     func setStyle() {
@@ -37,36 +29,3 @@ class AgreeVC: UIViewController {
     }
     
 }
-
-extension AgreeVC : UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: AgreeCVCell.identifier, for: indexPath) as? AgreeCVCell else{
-            return UICollectionViewCell()
-        }
-        return cell
-    }
-    
-    // MARK: - collectionView size
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let width = self.view.frame.width
-        let height =  collectionView.frame.height
-        
-        return CGSize(width: width, height: height)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout:
-                        UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    }
-    
-    
-}
-
--- wupitch/wupitch/Screen/TermsOfUse/Controllers/TermsOfUseVC.swift
+++ wupitch/wupitch/Screen/TermsOfUse/Controllers/TermsOfUseVC.swift
@@ -9,21 +9,13 @@ import UIKit
 
 class TermsOfUseVC: UIViewController {
     
-    @IBOutlet weak var contentCV: UICollectionView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var backBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        setDelegate()
         setStyle()
-    }
-    
-    func setDelegate() {
-        contentCV.delegate = self
-        contentCV.dataSource = self
-        contentCV.register(TouCVCell.nib(), forCellWithReuseIdentifier: TouCVCell.identifier)
     }
     
     func setStyle() {
@@ -38,34 +30,4 @@ class TermsOfUseVC: UIViewController {
     
 }
 
-extension TermsOfUseVC : UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TouCVCell.identifier, for: indexPath) as? TouCVCell else{
-            return UICollectionViewCell()
-        }
-        return cell
-    }
-    
-    // MARK: - collectionView size
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let width = self.view.frame.width
-        let height =  collectionView.frame.height
-        
-        return CGSize(width: width, height: height)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout:
-                        UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        
-        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    }
-    
-    
-}
+
