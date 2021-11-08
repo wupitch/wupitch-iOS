1f3a59595cb3e4836d1b3dfc8e31a64606b59977
sujeong <tnwjd6496@naver.com>
2021-11-09 02:31:18 +0900

    크루만들기 소개 페이지 틀 구현

--- wupitch/wupitch/Screen/Crew/Controllers/MakeCrewMoneyVC.swift
+++ wupitch/wupitch/Screen/Crew/Controllers/MakeCrewMoneyVC.swift
@@ -9,21 +9,15 @@ import UIKit
 
 class MakeCrewMoneyVC: UIViewController {
 
+    @IBOutlet weak var titleLabel: LabelFontSize!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setStyle()
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setStyle() {
+        titleLabel.makeCrewTitleLabel()
     }
-    */
 
 }
