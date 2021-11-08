3359cb1d344fd6ecdd7f7c4608d852b601019641
sujeong <tnwjd6496@naver.com>
2021-11-08 15:20:49 +0900

    전화번호 뷰 틀만 구현

---
+++ wupitch/wupitch/Screen/Crew/Controllers/MakeCrewMoneyVC.swift
@@ -0,0 +1,29 @@
+//
+//  MakeCrewMoneyVC.swift
+//  wupitch
+//
+//  Created by 김수정 on 2021/11/08.
+//
+
+import UIKit
+
+class MakeCrewMoneyVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
