6984709620f2e8d100054d95a565ca25519e8787
sujeong <tnwjd6496@naver.com>
2021-11-13 17:25:31 +0900

    크루 정기일정 뷰 껍데기 구현

--- wupitch/wupitch/Screen/Crew/Controllers/MakeCrewDateVC.swift
+++ wupitch/wupitch/Screen/Crew/Controllers/MakeCrewDateVC.swift
@@ -9,16 +9,46 @@ import UIKit
 
 class MakeCrewDateVC: UIViewController {
 
+    @IBOutlet weak var secondBgView: UIView!
+    @IBOutlet weak var firstBgView: UIView!
+    @IBOutlet var addBtn: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        addBtn[0].makeRounded(cornerRadius: nil)
+        addBtn[1].makeRounded(cornerRadius: nil)
     }
     
     @IBAction func touchUpNextBtn(_ sender: Any) {
         let storyBoard: UIStoryboard = UIStoryboard(name: "MakeCrewPhoto", bundle: nil)
         if let dvc = storyBoard.instantiateViewController(withIdentifier: "MakeCrewPhotoVC") as? MakeCrewPhotoVC {
             navigationController?.pushViewController(dvc, animated: true)
+        }
+    }
+    
+    @IBAction func touchUpAddBtn(_ sender: UIButton) {
+        if firstBgView.isHidden == false {
+            addBtn[0].setImage(UIImage(named: "subtract"), for: .normal)
+            firstBgView.isHidden = true
+            print("이프눌린다")
+        }
+        else {
+            addBtn[0].setImage(UIImage(named: "add"), for: .normal)
+            firstBgView.isHidden = false
+            print("엘즈눌린다")
+        }
+    }
+    
+    @IBAction func touchUpAddSecondBtn(_ sender: Any) {
+        if secondBgView.isHidden == false {
+            addBtn[1].setImage(UIImage(named: "subtract"), for: .normal)
+            secondBgView.isHidden = true
+            print("이프눌린다")
+        }
+        else {
+            addBtn[1].setImage(UIImage(named: "add"), for: .normal)
+            secondBgView.isHidden = false
+            print("엘즈눌린다")
         }
     }
     
