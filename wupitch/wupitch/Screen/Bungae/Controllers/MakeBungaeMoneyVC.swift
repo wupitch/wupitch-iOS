546e75fbad690c8a5a813dce8f1d20d01cfba0a3
sujeong <tnwjd6496@naver.com>
2021-11-24 00:04:28 +0900

    번개만들기 참여비 뷰 껍데기 구현

--- wupitch/wupitch/Screen/Bungae/Controllers/MakeBungaeMoneyVC.swift
+++ wupitch/wupitch/Screen/Bungae/Controllers/MakeBungaeMoneyVC.swift
@@ -9,21 +9,126 @@ import UIKit
 
 class MakeBungaeMoneyVC: UIViewController {
 
+    @IBOutlet weak var oneLabel: UILabel!
+    @IBOutlet weak var noMoneyLabel: CheckBtn!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var nextBtn: NextBtn!
+    @IBOutlet weak var titleLabel: LabelFontSize!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setStyle()
+        addDoneButtonOnKeyboard()
+    }
+
+    private func setStyle() {
+        oneLabel.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 16.adjusted)
+        oneLabel.textColor = .bk
+        titleLabel.makeCrewTitleLabel()
+        titleTextField.delegate = self
+        titleTextField.backgroundColor = .gray05
+        titleTextField.borderStyle = .none
+        titleTextField.makeRounded(cornerRadius: 8.adjusted)
+        titleTextField.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 16.adjusted)
+        titleTextField.textColor = .gray03
+        titleTextField.addLeftPadding()
+        
+        noMoneyLabel.setTitleColor(.gray02, for: .normal)
+        noMoneyLabel.titleLabel?.font = UIFont(name: "AppleSDGothicNeo-Regular", size: 14.adjusted)
+        noMoneyLabel.titleLabel?.setTextWithLineHeight(text: noMoneyLabel.titleLabel?.text, lineHeight: 19.adjusted)
+        
+        noMoneyLabel.grayCheck()
+    }
+    
+    func addDoneButtonOnKeyboard(){
+            let doneToolbar: UIToolbar = UIToolbar(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+            doneToolbar.barStyle = .default
+
+            let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+            let done: UIBarButtonItem = UIBarButtonItem(title: "확인", style: .done, target: self, action: #selector(self.doneButtonAction))
+
+            let items = [flexSpace, done]
+            doneToolbar.items = items
+            doneToolbar.sizeToFit()
+
+        titleTextField.inputAccessoryView = doneToolbar
+        }
+
+        @objc func doneButtonAction(){
+            titleTextField.resignFirstResponder()
+        }
+    
+    
+    @IBAction func touchUpNoMoneyBtn(_ sender: Any) {
+        if noMoneyLabel.status == false {
+            noMoneyLabel.colorCheck()
+            titleTextField.isEnabled = false
+            titleTextField.text = nil
+            nextBtn.backgroundColor = .main
+        }
+        else {
+            noMoneyLabel.grayCheck()
+            titleTextField.isEnabled = true
+            nextBtn.backgroundColor = .gray03
+        }
+    }
+    
+    @IBAction func touchUpBackBtn(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func touchUpCancelBtn(_ sender: Any) {
+        
+    }
+    
+    @IBAction func touchUpNextBtn(_ sender: Any) {
+        if nextBtn.backgroundColor == .main {
+            let storyBoard: UIStoryboard = UIStoryboard(name: "MakeCrewGuest", bundle: nil)
+            if let dvc = storyBoard.instantiateViewController(withIdentifier: "MakeCrewGuestVC") as? MakeCrewGuestVC {
+                navigationController?.pushViewController(dvc, animated: true)
+            }
+        }
+        else {
+            nextBtn.backgroundColor = .gray03
+        }
+    }
+
+}
+
+extension MakeBungaeMoneyVC : UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        titleTextField.textColor = .bk
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if titleTextField.text?.isEmpty == false {
+            nextBtn.backgroundColor = .main
+        }
+        else {
+            nextBtn.backgroundColor = .gray03
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        titleTextField.resignFirstResponder()
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        // 6자리로 막아놓기
+        if titleTextField.isEditing == true {
+            let currentCharacterCount = textField.text?.count ?? 0
+            if (range.length + range.location > currentCharacterCount){
+                return false
+            }
+            let newLength = currentCharacterCount + string.count - range.length
+            
+            return newLength <= 6
+        }
+        return true
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
