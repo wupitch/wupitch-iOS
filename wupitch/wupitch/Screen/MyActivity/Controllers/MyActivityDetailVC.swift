4424c00e83c40e7eaed887ea761604740d99cae5
suzumsz <tnwjd6496@naver.com>
2021-11-24 20:15:54 +0900

    내활동 디테일 뷰 수정중

---
+++ wupitch/wupitch/Screen/MyActivity/Controllers/MyActivityDetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  MyActivityDetailVC.swift
+//  wupitch
+//
+//  Created by 김수정 on 2021/11/24.
+//
+
+import UIKit
+
+class MyActivityDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+
+}
