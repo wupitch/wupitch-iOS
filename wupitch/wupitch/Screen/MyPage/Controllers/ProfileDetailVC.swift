45340ac5c839a1bdf89f2eb70a450ef4a9fbab51
suzumsz <tnwjd6496@naver.com>
2021-11-25 16:46:50 +0900

    마이페이지 프로필 닉네임 수정 뷰 연결

--- wupitch/wupitch/Screen/MyPage/Controllers/ProfileDetailVC.swift
+++ wupitch/wupitch/Screen/MyPage/Controllers/ProfileDetailVC.swift
@@ -30,6 +30,12 @@ class ProfileDetailVC: UIViewController {
     
     // 닉네임 및 소개 버튼
     @IBAction func touchUpNicknameBtn(_ sender: Any) {
+        
+        //버튼 클릭 시, 다음 스토리보드로 이동
+        let storyboard = UIStoryboard.init(name: "ProfileNickname", bundle: nil)
+        guard let dvc = storyboard.instantiateViewController(identifier: "ProfileNicknameVC") as? ProfileNicknameVC else {return}
+        self.tabBarController?.tabBar.isHidden = true
+        self.navigationController?.pushViewController(dvc, animated: true)
     }
     
     // 지역 버튼
